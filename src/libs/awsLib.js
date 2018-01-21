300e458f91b2f11ccae85fdbb8ee5eaf4db25aae
sohail87 <sohaildeen73@gmail.com>
2018-01-21 13:10:15 +0000

    invoke Api g

--- src/libs/awsLib.js
+++ src/libs/awsLib.js
@@ -1,6 +1,7 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 import AWS from "aws-sdk";
 import config from "../config";
+import sigV4Client from "./sigV4Client";
 
 function getAwsCredentials(userToken) {
   const authenticator = `cognito-idp.${config.cognito
@@ -67,4 +68,45 @@ export function signOutUser() {
   }
 }
 
+export async function invokeApig({
+  path,
+  method = "GET",
+  headers = {},
+  queryParams = {},
+  body
+}) {
+  if (!await authUser()) {
+    throw new Error("User is not logged in");
+  }
 
+  const signedRequest = sigV4Client
+    .newClient({
+      accessKey: AWS.config.credentials.accessKeyId,
+      secretKey: AWS.config.credentials.secretAccessKey,
+      sessionToken: AWS.config.credentials.sessionToken,
+      region: config.apiGateway.REGION,
+      endpoint: config.apiGateway.URL
+    })
+    .signRequest({
+      method,
+      path,
+      headers,
+      queryParams,
+      body
+    });
+
+  body = body ? JSON.stringify(body) : body;
+  headers = signedRequest.headers;
+
+  const results = await fetch(signedRequest.url, {
+    method,
+    headers,
+    body
+  });
+
+  if (results.status !== 200) {
+    throw new Error(await results.text());
+  }
+
+  return results.json();
+}
